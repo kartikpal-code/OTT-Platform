a48beef91b71aff11b97d930928e48a5db35c29b
kartikpal-code <kartikpal00123@gmail.com>
2025-11-22 21:27:40 +0530

    Finally done webseries pop-up

---
+++ webseries.js
@@ -0,0 +1,14 @@
+function openPopup(title, info, desc, img, link) {
+  document.getElementById("popup-title").innerText = title;
+  document.getElementById("popup-info").innerText = info;
+  document.getElementById("popup-desc").innerText = desc;
+  document.getElementById("popup-img").src = img;
+
+  document.getElementById("watchBtn").href = link;
+
+  document.getElementById("popup").style.display = "flex";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
